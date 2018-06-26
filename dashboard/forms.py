a27af696812e4280bb31c7b69db5e71035434aac
pramod <pga286478@gmail.com>
2018-06-26 17:12:33 +0530

    secondcommit

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -5,3 +5,9 @@ class Students_reg_form(forms.ModelForm):
 	class Meta:
 		model = models.DpyStudents
 		fields = '__all__'
+
+
+class Student_update_form(forms.ModelForm):
+	class Meta:
+		model = models.DpyStudents
+		fields = '__all__'	
--- dashboard/models.py
+++ dashboard/models.py
@@ -10,6 +10,7 @@ class DpyStudents(models.Model):
     roll_no = models.IntegerField()
     gr_no = models.IntegerField()
     dob = models.CharField(max_length=20)
+    email = models.CharField(max_length=100,null="true")
     blood_group = models.CharField(max_length=20)
     caste = models.CharField(max_length=20)
     nationality = models.CharField(max_length=20)
@@ -22,7 +23,8 @@ class DpyStudents(models.Model):
     gender = models.CharField(max_length=20)
     student_type = models.CharField(max_length=20)
     mobile = models.CharField(max_length=12)
-
+    student_class = models.CharField(max_length=12,default = "", null=True)
+    section = models.CharField(max_length=12,default = "", null=True)
     class Meta:
         managed = False
         db_table = 'dpy_students'
